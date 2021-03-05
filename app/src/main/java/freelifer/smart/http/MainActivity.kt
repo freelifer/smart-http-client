f6a9dcd4909f7ff152436176fb311ddd3e0e795a
kunzhu <576618121@qq.com>
2021-03-05 19:41:26 +0800

    first commit

---
+++ app/src/main/java/freelifer/smart/http/MainActivity.kt
@@ -0,0 +1,11 @@
+package freelifer.smart.http
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
